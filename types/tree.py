6e06d2b4f5ba5157b8ac920291c9ca6c2e3e31ae
Drayux <vcubefinatic@gmail.com>
2022-05-07 18:34:43 -0600

    Czar skeleton progress (will begin tree io when examples available)

---
+++ types/tree.py
@@ -0,0 +1,7 @@
+# Refresh of the ParseTree class from WRECK and ZOBOS
+# Houses an Abstract/Concrete Syntax Tree generated as a result
+#   of an LR parse
+
+class SyntaxTree:
+	def __init__(self, parent = None):
+		pass
